b3e53886627a482ceec9c7b1a26092e9a50f7104
shivammishra4519 <misra3408@gmail.com>
2024-04-20 19:46:26 +0530

    jhshshds

--- controler/pdf-files.js
+++ controler/pdf-files.js
@@ -1,142 +1,10 @@
-const puppeteer = require('puppeteer-core');
-const path = require('path'); const jwt = require('jsonwebtoken');
-const {getDB}=require('../dbconnection')
-require('dotenv').config();
-const url = process.env.frontEnd;
-const key = process.env.secretkey;
-const tokenExpiry = '12h'; // Token expiry set to 12 hours
-
-const downLoadTermsConditon = async (req, res) => {
-    try {
-        const number = req.query.number;
-        if (!number) {
-            return res.status(400).json({ error: 'invalid request' });
-        }
-
-        // Launch a headless browser instance
-        const browser = await puppeteer.launch({
-            executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-        });
-
-        // Open a new page
-        const page = await browser.newPage();
-
-        try {
-            // Navigate to the webpage
-            await page.goto(`${url}/terms-condtiton?order=${number}`, { waitUntil: 'networkidle2' });
-
-            // Set up the PDF options
-            const pdfOptions = {
-                format: 'A4', // Specify the format
-                printBackground: true, // Include background colors and images
-            };
-
-            // Generate the PDF as a buffer
-            const pdfBuffer = await page.pdf(pdfOptions);
-
-            // Set response headers for file download
-            res.setHeader('Content-Disposition', 'attachment; filename="terms_condition.pdf"');
-            res.setHeader('Content-Type', 'application/pdf');
-
-            res.send(pdfBuffer);
-        } catch (error) {
-            console.error('Error generating PDF:', error);
-            res.status(500).json({ error: 'Error generating PDF' });
-        } finally {
-            // Close the browser
-            await browser.close();
-        }
-    } catch (error) {
-        console.error('Internal server error:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-};
-
-
-
-const downLoadInstallmentSlip = async (req, res) => {
-    try {
-
-
-
-        const db = getDB();
-        const collection = db.collection('selldevice');
-        const data = req.query;
-
-        if (!data) {
-            return res.status(400).json({ error: 'invalid request' });
-        }
-        try {
-            const result = await collection.findOne({ loanId: data.loanId });
-          
-            if (!result) {
-                return res.status(404).json({ message: 'Loan not found' });
-            }
-            const installment = result.installments.find(installment => installment.installmentId === data.emiId);
-            
-            if (!installment) {
-                return res.status(404).json({ message: 'Installment not found' });
-            }
-            const browser = await puppeteer.launch({
-                executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-            });
-    
-            // Open a new page
-            const page = await browser.newPage();
-    
-            try {
-                // Navigate to the webpage
-                console.log('pdfBuffer')
-                // await page.goto(`http://62.72.56.135:4200/installment-slip/loanid=${data.loanId}&emiid=${data.emiId}`, { waitUntil: 'networkidle2' });
-                await page.goto(`${url}/installment-slip?loanid=${data.loanId}&emiid=${data.emiId}`, { waitUntil: 'networkidle2' });
-                // Set up the PDF options
-                const pdfOptions = {
-                    format: 'A4', // Specify the format
-                    printBackground: true, // Include background colors and images
-                };
-    
-                // Generate the PDF as a buffer
-                const pdfBuffer = await page.pdf(pdfOptions);
-                console.log(pdfBuffer)
-    
-                // Set response headers for file download
-                res.setHeader('Content-Disposition', 'attachment; filename="terms_condition.pdf"');
-                res.setHeader('Content-Type', 'application/pdf');
-    
-                res.send(pdfBuffer);
-            } catch (error) {
-                console.error('Error generating PDF:', error);
-                res.status(500).json({ error: 'Error generating PDF' });
-            } finally {
-                // Close the browser
-                await browser.close();
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            res.status(500).json({ message: 'Internal server error' });
-        }
-
-
-        // Launch a headless browser instance
-      
-
-    } catch (error) {
-        console.error('Internal server error:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-};
-
-module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
-
-
-
-
-
 // const puppeteer = require('puppeteer-core');
-// const { getDB } = require('../dbconnection');
+// const path = require('path'); const jwt = require('jsonwebtoken');
+// const {getDB}=require('../dbconnection')
 // require('dotenv').config();
 // const url = process.env.frontEnd;
-
+// const key = process.env.secretkey;
+// const tokenExpiry = '12h'; // Token expiry set to 12 hours
 
 // const downLoadTermsConditon = async (req, res) => {
 //     try {
@@ -145,26 +13,28 @@ module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
 //             return res.status(400).json({ error: 'invalid request' });
 //         }
 
+//         // Launch a headless browser instance
 //         const browser = await puppeteer.launch({
-//             executablePath: '/usr/bin/chromium-browser',
-//             args: ['--no-sandbox', '--disable-setuid-sandbox'], // Update with the correct path to Chrome on your VPS
+//             executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
 //         });
 
+//         // Open a new page
 //         const page = await browser.newPage();
 
 //         try {
-
-            
+//             // Navigate to the webpage
 //             await page.goto(`${url}/terms-condtiton?order=${number}`, { waitUntil: 'networkidle2' });
 
+//             // Set up the PDF options
 //             const pdfOptions = {
-//                 format: 'A4',
-//                 printBackground: true,
+//                 format: 'A4', // Specify the format
+//                 printBackground: true, // Include background colors and images
 //             };
 
-//             await page.addStyleTag({ content: 'body { font-family: "Noto Sans Devanagari", sans-serif; }' });
+//             // Generate the PDF as a buffer
 //             const pdfBuffer = await page.pdf(pdfOptions);
 
+//             // Set response headers for file download
 //             res.setHeader('Content-Disposition', 'attachment; filename="terms_condition.pdf"');
 //             res.setHeader('Content-Type', 'application/pdf');
 
@@ -173,6 +43,7 @@ module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
 //             console.error('Error generating PDF:', error);
 //             res.status(500).json({ error: 'Error generating PDF' });
 //         } finally {
+//             // Close the browser
 //             await browser.close();
 //         }
 //     } catch (error) {
@@ -181,8 +52,13 @@ module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
 //     }
 // };
 
+
+
 // const downLoadInstallmentSlip = async (req, res) => {
 //     try {
+
+
+
 //         const db = getDB();
 //         const collection = db.collection('selldevice');
 //         const data = req.query;
@@ -190,37 +66,41 @@ module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
 //         if (!data) {
 //             return res.status(400).json({ error: 'invalid request' });
 //         }
-
 //         try {
 //             const result = await collection.findOne({ loanId: data.loanId });
+          
 //             if (!result) {
 //                 return res.status(404).json({ message: 'Loan not found' });
 //             }
 //             const installment = result.installments.find(installment => installment.installmentId === data.emiId);
+            
 //             if (!installment) {
 //                 return res.status(404).json({ message: 'Installment not found' });
 //             }
-
 //             const browser = await puppeteer.launch({
-//                 executablePath: '/usr/bin/chromium-browser',
-//                 // executablePath: '/snap/bin/chromium',
-//                 args: ['--no-sandbox', '--disable-setuid-sandbox'],// Update with the correct path to Chrome on your VPS
+//                 executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
 //             });
     
+//             // Open a new page
 //             const page = await browser.newPage();
     
 //             try {
-                
+//                 // Navigate to the webpage
+//                 console.log('pdfBuffer')
+//                 // await page.goto(`http://62.72.56.135:4200/installment-slip/loanid=${data.loanId}&emiid=${data.emiId}`, { waitUntil: 'networkidle2' });
 //                 await page.goto(`${url}/installment-slip?loanid=${data.loanId}&emiid=${data.emiId}`, { waitUntil: 'networkidle2' });
-    
+//                 // Set up the PDF options
 //                 const pdfOptions = {
-//                     format: 'A4',
-//                     printBackground: true,
+//                     format: 'A4', // Specify the format
+//                     printBackground: true, // Include background colors and images
 //                 };
-//                 await page.addStyleTag({ content: 'body { font-family: "Noto Sans Devanagari", sans-serif; }' });
-//                 const pdfBuffer = await page.pdf(pdfOptions);
     
-//                 res.setHeader('Content-Disposition', 'attachment; filename="installment-slip.pdf"');
+//                 // Generate the PDF as a buffer
+//                 const pdfBuffer = await page.pdf(pdfOptions);
+//                 console.log(pdfBuffer)
+    
+//                 // Set response headers for file download
+//                 res.setHeader('Content-Disposition', 'attachment; filename="terms_condition.pdf"');
 //                 res.setHeader('Content-Type', 'application/pdf');
     
 //                 res.send(pdfBuffer);
@@ -228,12 +108,18 @@ module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
 //                 console.error('Error generating PDF:', error);
 //                 res.status(500).json({ error: 'Error generating PDF' });
 //             } finally {
+//                 // Close the browser
 //                 await browser.close();
 //             }
 //         } catch (error) {
 //             console.error('Error:', error);
 //             res.status(500).json({ message: 'Internal server error' });
 //         }
+
+
+//         // Launch a headless browser instance
+      
+
 //     } catch (error) {
 //         console.error('Internal server error:', error);
 //         res.status(500).json({ error: 'Internal server error' });
@@ -241,4 +127,118 @@ module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
 // };
 
 // module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
+
+
+
+
+
+const puppeteer = require('puppeteer-core');
+const { getDB } = require('../dbconnection');
+require('dotenv').config();
+const url = process.env.frontEnd;
+
+
+const downLoadTermsConditon = async (req, res) => {
+    try {
+        const number = req.query.number;
+        if (!number) {
+            return res.status(400).json({ error: 'invalid request' });
+        }
+
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/chromium-browser',
+            args: ['--no-sandbox', '--disable-setuid-sandbox'], // Update with the correct path to Chrome on your VPS
+        });
+
+        const page = await browser.newPage();
+
+        try {
+
+            
+            await page.goto(`${url}/terms-condtiton?order=${number}`, { waitUntil: 'networkidle2' });
+
+            const pdfOptions = {
+                format: 'A4',
+                printBackground: true,
+            };
+
+            await page.addStyleTag({ content: 'body { font-family: "Noto Sans Devanagari", sans-serif; }' });
+            const pdfBuffer = await page.pdf(pdfOptions);
+
+            res.setHeader('Content-Disposition', 'attachment; filename="terms_condition.pdf"');
+            res.setHeader('Content-Type', 'application/pdf');
+
+            res.send(pdfBuffer);
+        } catch (error) {
+            console.error('Error generating PDF:', error);
+            res.status(500).json({ error: 'Error generating PDF' });
+        } finally {
+            await browser.close();
+        }
+    } catch (error) {
+        console.error('Internal server error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+const downLoadInstallmentSlip = async (req, res) => {
+    try {
+        const db = getDB();
+        const collection = db.collection('selldevice');
+        const data = req.query;
+
+        if (!data) {
+            return res.status(400).json({ error: 'invalid request' });
+        }
+
+        try {
+            const result = await collection.findOne({ loanId: data.loanId });
+            if (!result) {
+                return res.status(404).json({ message: 'Loan not found' });
+            }
+            const installment = result.installments.find(installment => installment.installmentId === data.emiId);
+            if (!installment) {
+                return res.status(404).json({ message: 'Installment not found' });
+            }
+
+            const browser = await puppeteer.launch({
+                executablePath: '/usr/bin/chromium-browser',
+                // executablePath: '/snap/bin/chromium',
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],// Update with the correct path to Chrome on your VPS
+            });
+    
+            const page = await browser.newPage();
+    
+            try {
+                
+                await page.goto(`${url}/installment-slip?loanid=${data.loanId}&emiid=${data.emiId}`, { waitUntil: 'networkidle2' });
+    
+                const pdfOptions = {
+                    format: 'A4',
+                    printBackground: true,
+                };
+                await page.addStyleTag({ content: 'body { font-family: "Noto Sans Devanagari", sans-serif; }' });
+                const pdfBuffer = await page.pdf(pdfOptions);
+    
+                res.setHeader('Content-Disposition', 'attachment; filename="installment-slip.pdf"');
+                res.setHeader('Content-Type', 'application/pdf');
+    
+                res.send(pdfBuffer);
+            } catch (error) {
+                console.error('Error generating PDF:', error);
+                res.status(500).json({ error: 'Error generating PDF' });
+            } finally {
+                await browser.close();
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    } catch (error) {
+        console.error('Internal server error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+module.exports = { downLoadTermsConditon, downLoadInstallmentSlip };
 
